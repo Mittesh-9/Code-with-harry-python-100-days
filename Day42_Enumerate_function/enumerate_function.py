2ea848b869b120de9048020e4095c2b1b0ee5a88
Mittesh <mitteshwaghule09@gmail.com>
2023-11-12 11:00:27 +0530

    added Day42

---
+++ Day42_Enumerate_function/enumerate_function.py
@@ -0,0 +1,20 @@
+# fruits = ['apple','banana','mango','guava']
+
+# for index, fruit in enumerate(fruits):
+#     print(index, fruit)
+
+marks = [12,34,1,4,56,98,14,3,77]
+
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if (index == 5):
+#         print("Mittesh awesome!")
+#     index += 1
+
+# agar aap chahte ho ki is upar waale code ko aasaani se kiya jaa sake toh ye raha example
+
+for index, mark in enumerate(marks):
+    print(mark)
+    if (index == 5):
+        print("Mittesh awesome!")
