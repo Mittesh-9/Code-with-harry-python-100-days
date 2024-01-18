d2626474ec3c80d7268b7b4f1cda56daf57f0323
Mittesh <mitteshwaghule09@gmail.com>
2024-01-18 18:18:07 +0530

    added Day56

---
+++ Day56_Introduction_to_OOP/main.py
@@ -0,0 +1,6 @@
+"""
+RailwayForm   ---> Class [blueprint]
+Mittesh --> mittesh ki info wala form --> Object [entity]
+harry --> harry ki info wala form --> Object [entity]
+tom --> tom ki info wala form --> Object [entity]
+"""
